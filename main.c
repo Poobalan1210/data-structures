8ea74cf4bdd66ff2b6878bba10ed070a50cc1952
Poobalan <53970468+Poobalan1210@users.noreply.github.com>
2019-12-11 15:30:47 +0530

    Update and rename README.md to main.c
    c program to insert and delete element in list

---
+++ main.c
@@ -0,0 +1,134 @@
+#include<stdio.h>
+#include<stdlib.h>
+ 
+struct node
+{
+    int data;
+    struct node *next;
+}*head;
+ 
+ 
+
+ 
+ 
+void add( int num )
+{
+    struct node *temp;
+    temp=(struct node *)malloc(sizeof(struct node));
+    temp->data=num;
+    if (head== NULL)
+    {
+    head=temp;
+    head->next=NULL;
+    }
+    else
+    {
+    temp->next=head;
+    head=temp;
+    }
+}
+
+ 
+ 
+void insert(int num)
+{
+    int c=0;
+    struct node *temp;
+    temp=head;
+    if(temp==NULL)
+    {
+    add(num);
+    }
+    else
+    {
+    while(temp!=NULL)
+    {
+        if(temp->data<num)
+        c++;
+        temp=temp->next;
+    }
+    if(c==0)
+        add(num);
+    }
+}
+ 
+ 
+ 
+int delete(int num)
+{
+    struct node *temp, *prev;
+    temp=head;
+    while(temp!=NULL)
+    {
+    if(temp->data==num)
+    {
+        if(temp==head)
+        {
+        head=temp->next;
+        free(temp);
+        return 1;
+        }
+        else
+        {
+        prev->next=temp->next;
+        free(temp);
+        return 1;
+        }
+    }
+    else
+    {
+        prev=temp;
+        temp= temp->next;
+    }
+    }
+    return 0;
+}
+ 
+ 
+
+ 
+ 
+int  main()
+{
+    int i,num;
+    struct node *n;
+    head=NULL;
+    while(1)
+    {
+    printf("\nList Operations\n");
+    printf("===============\n");
+    printf("1.Insert\n");
+    
+    printf("4.Delete\n");
+    
+    printf("Enter your choice : ");
+    if(scanf("%d",&i)<=0){
+        printf("Enter only an Integer\n");
+        exit(0);
+    } else {
+        switch(i)
+        {
+        case 1:      printf("Enter the number to insert : ");
+                 scanf("%d",&num);
+                 insert(num);
+                 break;
+        
+        
+        case 4:     if(head==NULL)
+                printf("List is Empty\n");
+                else{
+                printf("Enter the number to delete : ");
+                scanf("%d",&num);
+                if(delete(num))
+                    printf("%d deleted successfully\n",num);
+                else
+                    printf("%d not found in the list\n",num);
+                }
+                break;
+        
+        default:    printf("Invalid option\n");
+        }
+    }
+    }
+    return 0;
+}
